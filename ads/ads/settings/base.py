1153b20ad5f59464b149cb6a193f2198bef64657
owner <angelichka21.02@gmail.com>
2022-11-10 23:45:53 +0300

    test SPA with vue js

--- ads/ads/settings/base.py
+++ ads/ads/settings/base.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'bootstrap4',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <main>
+    <div v-for="item in data">Alloha: {{item.name}}</div>
+  </main>
+</template>
+
+  <script setup lang="ts">
+  import axois from 'axios';
+import { ref } from 'vue';
+const data = ref([]);
+
+  axois.get('http://localhost:8000/api/rubric/').then((dataresp)=>{
+data.value = dataresp.data; 
+  })
+  .catch(error=>{
+    console.log('alloha: ', error); 
+   })
+  </script>
