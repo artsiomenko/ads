67dd115c12f7bf224ad5dac0ba90df44dbabdc0e
aya <aya@anadeainc.com>
2022-11-11 15:49:33 +0300

    create services

--- ads/main/views.py
+++ ads/main/views.py
@@ -10,6 +10,7 @@ from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 from .models import Ad, Rubric
 from .services.search import search
+from .services.user_create_ad import user_create_ad
 
 
 def index(request):
@@ -74,15 +75,5 @@ def profile_ad_detail(request, rubric_name, ad_id):
 @login_required
 def profile_new_ad(request):
     rubrics = Rubric.objects.all()
-    if request.method == 'POST':
-        form = AdForm(request.POST)
-        if form.is_valid():
-            form.save()
-            ads = Ad.objects.filter(author=request.user.pk)
-            context = {'form': form, 'ads': ads, 'rubrics': rubrics}
-            return render(request, 'main/profile.html', context)
-    else:
-        form = AdForm(initial={'author': request.user.pk})
-        ads = Ad.objects.filter(author=request.user.pk)
-        context = {'form': form, 'ads': ads, 'rubrics': rubrics}
+    context = user_create_ad(request, rubrics)
     return render(request, 'main/profile_new_ad.html', context)
