02b82675a0a74b9cbad416fed03ec7a3e549270f
aya <aya@anadeainc.com>
2022-11-16 13:31:16 +0300

    test query params for ads

--- ads/api/tests/test_ads_query_params.py
+++ ads/api/tests/test_ads_query_params.py
@@ -1,20 +1,35 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.test import Client
-import requests
-from api.tests.factories import AdFactory, AdFactory2
+from .factories import AdFactory, AdFactory2
+from .options import *
 
 
-class ViewPageAdsByRubric(StaticLiveServerTestCase):
+class UserSearchQuery(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = Options()
+        options.add_argument('--headless')
+        cls.selenium = WebDriver(chrome_options=options)
+
     def test_query_params(self):
-        client = Client()
-        ad, ad2 = AdFactory(), AdFactory2()
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?rubric=1')).text)
-        self.assertTrue('House' in page_text and 'Car' not in page_text)
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?rubric=2')).text)
-        self.assertTrue('Car' in page_text and 'House' not in page_text)
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?author=1')).text)
-        self.assertTrue('House' in page_text and 'Car' not in page_text)
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?author=2')).text)
-        self.assertTrue('Car' in page_text and 'House' not in page_text)
+        AdFactory(), AdFactory2()
+        # test_by_rubric
+        self.selenium.get(self.live_server_url + '/api/ads/?rubric=1')
+        assert 'House' in self.selenium.page_source and 'Car' not in self.selenium.page_source
 
+        self.selenium.get(self.live_server_url + '/api/ads/?rubric=2')
+        assert 'Car' in self.selenium.page_source and 'House' not in self.selenium.page_source
+        # test_by_author
+        self.selenium.get(self.live_server_url + '/api/ads/?author=1')
+        assert 'House' in self.selenium.page_source and 'Car' not in self.selenium.page_source
 
+        self.selenium.get(self.live_server_url + '/api/ads/?author=2')
+        assert 'Car' in self.selenium.page_source and 'House' not in self.selenium.page_source
+        # test_by_author_and_rubric
+        self.selenium.get(self.live_server_url + '/api/ads/?author=2&rubric=2')
+        assert 'Car' in self.selenium.page_source and 'House' not in self.selenium.page_source
+
+        self.selenium.get(self.live_server_url + '/api/ads/?author=1&rubric=2')
+        assert 'Car' not in self.selenium.page_source and 'House' not in self.selenium.page_source
+
+    def tearDown(self):
+        self.selenium.quit()
