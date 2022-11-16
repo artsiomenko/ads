cd5eca3f27886e611581cf46209f71b438c00c1e
aya <aya@anadeainc.com>
2022-11-16 10:07:51 +0300

    AdsQueryParamsServices

--- ads/api/tests/test_ads_query_params.py
+++ ads/api/tests/test_ads_query_params.py
@@ -8,13 +8,13 @@ class ViewPageAdsByRubric(StaticLiveServerTestCase):
     def test_query_params(self):
         client = Client()
         ad, ad2 = AdFactory(), AdFactory2()
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?rubric_id=1')).text)
+        page_text = str((requests.get(self.live_server_url + '/api/ads/?rubric=1')).text)
         self.assertTrue('House' in page_text and 'Car' not in page_text)
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?rubric_id=2')).text)
+        page_text = str((requests.get(self.live_server_url + '/api/ads/?rubric=2')).text)
         self.assertTrue('Car' in page_text and 'House' not in page_text)
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?author_id=1')).text)
+        page_text = str((requests.get(self.live_server_url + '/api/ads/?author=1')).text)
         self.assertTrue('House' in page_text and 'Car' not in page_text)
-        page_text = str((requests.get(self.live_server_url + '/api/ads/?author_id=2')).text)
+        page_text = str((requests.get(self.live_server_url + '/api/ads/?author=2')).text)
         self.assertTrue('Car' in page_text and 'House' not in page_text)
 
 
