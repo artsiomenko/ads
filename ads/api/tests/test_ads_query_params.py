7453dafe3dcd997ff0b3debbbb784d9e6ef62c2c
aya <aya@anadeainc.com>
2022-11-17 15:55:36 +0300

    test new ad POST

--- ads/api/tests/test_ads_query_params.py
+++ ads/api/tests/test_ads_query_params.py
@@ -10,6 +10,10 @@ class UserSearchQuery(StaticLiveServerTestCase):
         options.add_argument('--headless')
         cls.selenium = WebDriver(chrome_options=options)
 
+    @classmethod
+    def tearDown(cls):
+        cls.selenium.quit()
+
     def test_query_params(self):
         AdFactory(), AdFactory2()
         # test_by_rubric
@@ -27,7 +31,9 @@ class UserSearchQuery(StaticLiveServerTestCase):
         assert 'Car' in self.selenium.page_source and 'House' not in self.selenium.page_source
         self.selenium.get(self.live_server_url + '/api/ads/?author=1&rubric=2')
         assert 'Car' not in self.selenium.page_source and 'House' not in self.selenium.page_source
+        # test_by_keyword
+        self.selenium.get(self.live_server_url + '/api/ads/?keyword=House')
+        assert 'Car' not in self.selenium.page_source and 'House' in self.selenium.page_source
 
-    @classmethod
-    def tearDown(cls):
-        cls.selenium.quit()
+
+
---
+++ ads/api/tests/test_new_ad_post.py
@@ -0,0 +1,26 @@
+from .options import *
+
+
+class TestNewAdPost(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = Options()
+        options.add_argument('--headless')
+        cls.selenium = WebDriver(chrome_options=options)
+
+    @classmethod
+    def tearDown(cls):
+        cls.selenium.quit()
+
+    def test_new_ad_post(self):
+        self.selenium.get(self.live_server_url + '/ads/')
+        title = self.selenium.find_element(By.NAME, 'title')
+        title.send_keys('title')
+        content = self.selenium.find_element(By.NAME, 'content')
+        content.send_keys('content')
+        price = self.selenium.find_element(By.NAME, 'price')
+        price.send_keys(120000)
+        add_button = self.selenium.find_element(By.XPATH, '//*[@id="post-object-form"]/form/fieldset/div[4]/button')
+        add_button.click()
+        assert 'Title' in self.selenium.page_source
